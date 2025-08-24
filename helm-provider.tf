7dcaef24c62be8058e9bd6b3e87c2af92ffa781a
lateef-taiwo <taiwolateef55@gmail.com>
2025-08-23 22:33:34 -0500

    Added new terraform files and k8s manifest files

--- helm-provider.tf
+++ helm-provider.tf
@@ -7,10 +7,10 @@ data "aws_eks_cluster_auth" "eks_cluster" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = data.aws_eks_cluster.eks_cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.eks_cluster.token
   }
-  
+
 }
--- metrics-server.tf
+++ metrics-server.tf
@@ -1,12 +1,12 @@
 resource "helm_release" "metrics_server" {
-  name       = "metrics-server"
+  name = "metrics-server"
 
   repository = "https://kubernetes-sigs.github.io/metrics-server"
   chart      = "metrics-server"
-  namespace = "kube-system"
+  namespace  = "kube-system"
   version    = "3.12.1"
 
   values = [file("${path.module}/values/metrics-server.yaml")]
 
-  depends_on = [aws_eks_node_group.general]
+  depends_on = [aws_eks_node_group.eks-worker-nodes]
 }
